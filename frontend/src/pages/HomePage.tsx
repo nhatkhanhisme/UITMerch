29e5fd75ba02ada7257134d80f629ce18be1c5d2
QUOC HAI <haiquocntp@gmail.com>
2026-05-11 14:53:26 +0700

    fix: hide the default slider in HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { HomeEnd } from "../components/home/HomeEnd";
 import { AmbientBackgroundGradients } from "../components/home/AmbientBackgroundGradients";
 import { HomeFixedChrome } from "../components/home/HomeFixedChrome";
@@ -7,6 +8,16 @@ import { HomeOrgan } from "../components/home/HomeOrgan";
 import { HomeWhy } from "../components/home/HomeWhy";
 
 export function HomePage() {
+  useEffect(() => {
+    document.documentElement.classList.add("home-hide-native-scrollbar");
+    document.body.classList.add("home-hide-native-scrollbar");
+
+    return () => {
+      document.documentElement.classList.remove("home-hide-native-scrollbar");
+      document.body.classList.remove("home-hide-native-scrollbar");
+    };
+  }, []);
+
   return (
     <>
       <HomeFixedChrome />
